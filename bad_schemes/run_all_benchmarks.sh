96f869e65efd4083994613b4eb8324df36294842
adelapie <antonio@delapiedra.org>
2022-01-11 18:04:24 +0100

    added more information in performance output

--- bad_schemes/run_all_benchmarks.sh
+++ bad_schemes/run_all_benchmarks.sh
@@ -4,6 +4,9 @@
 echo "bad-i"
 for i in 1 5 10 20 30 40 50 60 70 80 90 100; do ./bad_i $i ; done;
 
+echo
+echo
+
 echo "bad-ii"
 for i in 1 5 10 20 30 40 50 60 70 80 90 100; do ./bad_ii $i ; done;
 
